fc2628bae4b6311deca2bfdebc8acbb74f22a127
stefanhallermann <stefan_jens.hallermann@uni-leipzig.de>
2025-11-11 19:28:34 +0100

    test config.py ignore

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ EXTERNAL_DATA_FOLDER = "/Users/stefanhallermann/Desktop/tmp/RinakoData"
 IMPORT_FOLDER = os.path.join(ROOT_FOLDER, "in")
 METADATA_FILE = os.path.join(IMPORT_FOLDER, "metadata.xlsx")
 
-MY_INITIAL = "SH"
+MY_INITIAL = "SH_test"
